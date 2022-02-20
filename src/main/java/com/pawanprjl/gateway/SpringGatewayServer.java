599aee7e9e0c463455ec153a3a786b852db7f461
pawanprjl <pawanparajuli8@gmail.com>
2022-02-20 17:27:58 +0545

    initial commit

---
+++ src/main/java/com/pawanprjl/gateway/SpringGatewayServer.java
@@ -0,0 +1,13 @@
+package com.pawanprjl.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGatewayServer.class, args);
+    }
+
+}
---
+++ src/test/java/com/pawanprjl/gateway/SpringGatewayServerTests.java
@@ -0,0 +1,13 @@
+package com.pawanprjl.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayServerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
